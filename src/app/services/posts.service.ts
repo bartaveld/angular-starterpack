e14429210a057b6440b81bf184ba207fddecba56
Bart in 't Veld <bart.int.007@gmail.com>
2017-12-13 10:12:04 +0100

    Removed console error

--- src/app/services/posts.service.ts
+++ src/app/services/posts.service.ts
@@ -24,24 +24,28 @@ export class PostsService {
     this.http.get(this.serverUrl + '/posts/followers', { headers: this.headerService.getHeaders()})
       .toPromise()
       .then(response => {
-        const responseJson = response.json();
-        if (this.savedPost === undefined || this.savedPost.id !== responseJson[0]._id || response.status === 200 ) {
-          const postsList = [];
-          responseJson.forEach((post, index) => {
-            const newPost = new Post({id: post._id, title: post.title, message: post.message, username: post.user, postedOn: new Date(post.createdAt)});
-            const comments = [];
-            post.comments.forEach(comment => {
-              comments.unshift(new Comment({id: comment._id, username: comment.user, message: comment.message, postedOn: new Date(comment.createdAt)}));
+        if(response.status === 204) {
+          console.log('No content')
+        } else {
+          const responseJson = response.json();
+          if (this.savedPost === undefined || this.savedPost.id !== responseJson[0]._id || response.status === 200 ) {
+            const postsList = [];
+            responseJson.forEach((post, index) => {
+              const newPost = new Post({id: post._id, title: post.title, message: post.message, username: post.user, postedOn: new Date(post.createdAt)});
+              const comments = [];
+              post.comments.forEach(comment => {
+                comments.unshift(new Comment({id: comment._id, username: comment.user, message: comment.message, postedOn: new Date(comment.createdAt)}));
+              });
+              newPost.comments = comments;
+  
+              if (index === 0) {
+                this.savedPost = newPost;
+              }
+              postsList.push(newPost);
             });
-            newPost.comments = comments;
-
-            if (index === 0) {
-              this.savedPost = newPost;
-            }
-            postsList.push(newPost);
-          });
-          this.savedPosts = postsList;
-          this.posts.next(postsList);
+            this.savedPosts = postsList;
+            this.posts.next(postsList);
+          }
         }
       })
       .catch(error => {
